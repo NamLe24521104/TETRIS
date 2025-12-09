593a1249870c8bba03d6ab4e9efb76e262e8e1fa
Lê Hoài Nam <24521104@gm.uit.edu.vn>
2025-12-09 19:51:21 +0700

    Khung sườn cơ bản gồm tên các hàm

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void enableRawMode() {}
+
+using namespace std;
+
+void boardDelBlock(){}
+
+void block2Board(){}
+
+void draw(){}
+
+bool canMove() {}
+
+bool canRotate() {}
+
+void rotateBlock() {}
+
+bool isGameOver() {}
+
+
+int main(){
+return 0;
+
+}
